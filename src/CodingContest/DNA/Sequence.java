ef238b9f79ba3300e84e9e6d6a1004222cba8c1e
Marcel <makisi@gmx.de>
2013-05-08 01:40:54 +0200

    First commit

---
+++ src/CodingContest/DNA/Sequence.java
@@ -0,0 +1,11 @@
+package CodingContest.DNA;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: makisi
+ * Date: 08.05.13 (19)
+ * Time: 01:14
+ * To change this template use File | Settings | File Templates.
+ */
+public class Sequence {
+}
